4dbaf9421a5e380fd8c62e00b4c1634ad1479a9c
Aman Srivastava <47601036+aman-5757@users.noreply.github.com>
2019-12-26 21:24:49 +0530

    countDigit.java
    Question
    1. You've to count the number of digits in a number.
    2. Take as input "n", the number for which the digits have to be counted.
    3. Print the digits in that number.
    
    Input Format
    "n" where n is an integer.
    
    Output Format
    "d" where d is the number of digits in the number "n"
    
    Constraints
    1 <= n < 10^9
    
    Sample Input
    65784383
    
    Sample Output
    8

--- countDigit.java
+++ countDigit.java
@@ -13,3 +13,24 @@ import java.util.*;
         System.out.print(d);
     }
     }
+
+
+// Question
+// 1. You've to count the number of digits in a number.
+// 2. Take as input "n", the number for which the digits have to be counted.
+// 3. Print the digits in that number.
+
+// Input Format
+// "n" where n is an integer.
+
+// Output Format
+// "d" where d is the number of digits in the number "n"
+
+// Constraints
+// 1 <= n < 10^9
+
+// Sample Input
+// 65784383
+
+// Sample Output
+// 8
