aa992ea77f32598b1bed9d87f4675181dd7aa2a8
Aman Srivastava <47601036+aman-5757@users.noreply.github.com>
2019-12-26 18:14:32 +0530

    Add files via upload

---
+++ primeOrNot.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class q{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+  
+        int t= scn.nextInt();
+        
+        for (int i=1;i<=t;i++){
+            int c=0;
+            int n = scn.nextInt();
+            for(int j=2; j*j<=n;j++){
+                if(n%j==0){
+                    c++;
+                    break;
+                }
+            }if (c!=0){
+                System.out.print("not prime");
+            }
+            else    System.out.print("prime");
+            
+        }
+   }
+  }
